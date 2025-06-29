b02a8997650fd4c76fca8b428ade448b273f9ce7
Athul N (INFOSYS LIMITED) <v-nathul@microsoft.com>
2025-06-29 23:45:16 +0530

    Fix: Added redirect f

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build:{outDir: 'dist', // this must match what Render expects
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000', // 👈 match your backend port
