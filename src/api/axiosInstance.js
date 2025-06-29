3f0587a7b3ae7bf643969fda8bc4386a407b445f
Athul N (INFOSYS LIMITED) <v-nathul@microsoft.com>
2025-06-30 02:04:19 +0530

    Final deployment fix

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-const url=import.meta.env.VITE_API_BASE_URL
+//const url=import.meta.env.VITE_API_BASE_URL
 export const  api =axios.create({
-    baseURL:`${url}/api`,
+    baseURL:'https://project-backend-1-jpy2.onrender.com/api',
     
     
 
